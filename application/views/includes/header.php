21dbf6fa43c91ade984dc1c48b20fa2d7c5316ff
XXX <gabz.fernandez96@gmail.com>
2018-11-12 10:45:18 +0800

    Header adjustments

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -19,8 +19,8 @@
 
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+<nav class="header navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">VISITA</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
