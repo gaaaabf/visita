14e57c147fe5d8fc6fe9bcc2096270453ae028c7
XXX <gabz.fernandez96@gmail.com>
2018-11-13 17:00:42 +0800

    Title Added

--- application/views/home.php
+++ application/views/home.php
@@ -1,72 +1,76 @@
 
-	</nav>
 
-	<div class="container">
-		<div class="jumbotron" style="bottom: 50px;" >
+<div class="container">
+	<div class="jumbotron" style="bottom: 50px;" >
+			<i class="fa fa-camera-retro fa-5x"></i>
+		<h1 style="display:inline-block;">VISITA</h1>
+	</div>		
 
- 			<i class="fa fa-camera-retro fa-5x"></i>
-			<h1 style="margin-top: 5px;position: relative;text-shadow: -2px -1px white; display: inline-block;">VISITA</h1>
-		</div>		
+<!-- 	<div>	
+		<p><a href="#">Become a TOUR GUIDE</a></p>
+	</div> -->
 
 	<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block cs-carousel w-100" src="<?php echo base_url('assets/images/PLACES/FORTSANPEDRO.jpg'); ?>" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block cs-carousel w-100" src="<?php echo base_url('assets/images/PLACES/HERITAGEOFCEBUMONUMENT2.jpg'); ?>" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block cs-carousel w-100" src="<?php echo base_url('assets/images/PLACES/KAWASAN.jpg'); ?>" alt="Third slide">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-	</div>
+		<h1 class="cs-title">VISIT THE BEST SPOTS IN CEBU</h1>
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+			  <img class="d-block cs-carousel w-100" src="<?php echo base_url('assets/images/PLACES/FORTSANPEDRO.jpg'); ?>" alt="First slide">
+			</div>
+			<div class="carousel-item">
+			  <img class="d-block cs-carousel w-100" src="<?php echo base_url('assets/images/PLACES/HERITAGEOFCEBUMONUMENT2.jpg'); ?>" alt="Second slide">
+			</div>
+			<div class="carousel-item">
+			  <img class="d-block cs-carousel w-100" src="<?php echo base_url('assets/images/PLACES/KAWASAN.jpg'); ?>" alt="Third slide">
+			</div>
+		</div>
 
-  <section class="gallery-block compact-gallery">
-        <div class="container">
-            <div class="heading">
-                <h2>FEATURED POSTS</h2>
-            </div>
-            <div class="row no-gutters">
-                <div class="col-md-6 col-lg-4 item zoom-on-hover">
-                    <a class="lightbox" href="assets/images/img/image2.jpg">
-                        <img class="img-fluid image"  src="<?php echo base_url('assets/images/img/image2.jpg'); ?>">
-                        <span class="description">
-                            <span class="description-heading">Lorem Ipsum</span>
-                            <span class="description-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
-                        </span>
-                    </a>
-                </div>
-                <div class="col-md-6 col-lg-4 item zoom-on-hover">
-                    <a class="lightbox" href="assets/images/img/image3.jpg">
-                        <img class="img-fluid image" src="<?php echo base_url('assets/images/img/image3.jpg'); ?>">
-                        <span class="description">
-                            <span class="description-heading">Lorem Ipsum</span>
-                            <span class="description-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
-                        </span>
-                    </a>
-                </div>
-                <div class="col-md-6 col-lg-4 item zoom-on-hover">
-                    <a class="lightbox" href="assets/images/img/image4.jpg">
-                        <img class="img-fluid image"  src="<?php echo base_url('assets/images/img/image4.jpg'); ?>">
-                        <span class="description">
-                            <span class="description-heading">Lorem Ipsum</span>
-                            <span class="description-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
-                        </span>
-                    </a>
-                </div>
-                
-            </div>
+		<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	</div>
+</div>
+
+<section class="gallery-block compact-gallery">
+    <div class="container">
+        <div class="heading">
+            <h2>FEATURED POSTS</h2>
         </div>
-    </section>
+        <div class="row no-gutters">
+            <div class="col-md-6 col-lg-4 item zoom-on-hover">
+                <a class="lightbox" href="assets/images/img/image2.jpg">
+                    <img class="img-fluid image"  src="<?php echo base_url('assets/images/img/image2.jpg'); ?>">
+                    <span class="description">
+                        <span class="description-heading">Lorem Ipsum</span>
+                        <span class="description-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+                    </span>
+                </a>
+            </div>
+            <div class="col-md-6 col-lg-4 item zoom-on-hover">
+                <a class="lightbox" href="assets/images/img/image3.jpg">
+                    <img class="img-fluid image" src="<?php echo base_url('assets/images/img/image3.jpg'); ?>">
+                    <span class="description">
+                        <span class="description-heading">Lorem Ipsum</span>
+                        <span class="description-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+                    </span>
+                </a>
+            </div>
+            <div class="col-md-6 col-lg-4 item zoom-on-hover">
+                <a class="lightbox" href="assets/images/img/image4.jpg">
+                    <img class="img-fluid image"  src="<?php echo base_url('assets/images/img/image4.jpg'); ?>">
+                    <span class="description">
+                        <span class="description-heading">Lorem Ipsum</span>
+                        <span class="description-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+                    </span>
+                </a>
+            </div>
+            
+        </div>
+    </div>
+</section>
 
   
