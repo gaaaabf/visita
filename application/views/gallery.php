4781e2f7b2b2ff7922943fa617de35057cdba750
XXX <gabz.fernandez96@gmail.com>
2018-11-13 16:34:37 +0800

    Indentions

--- application/views/gallery.php
+++ application/views/gallery.php
@@ -1,85 +1,78 @@
-    
-
-
-
-
 <div class="container">
 
   <h1 class="my-4 text-center text-lg-left">Thumbnail Gallery</h1>
-  
+
   <div class="dropdown">
-    <button type="button" class="btn dropdown-toggle" data-toggle="dropdown">
-      Categories
-    </button>
-    <div class="dropdown-menu">
-      <a class="dropdown-item" href="#">Link 1</a>
-      <a class="dropdown-item" href="#">Link 2</a>
-      <a class="dropdown-item" href="#">Link 3</a>
-    </div>
-</div>
-
-      <div class="row text-center text-lg-left">
-
-        <div class="col-lg-3 col-md-4 col-xs-6">
-          <a href="#" class="d-block mb-4 h-100">
-            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
-          </a>
-        </div>
-        <div class="col-lg-3 col-md-4 col-xs-6">
-          <a href="#" class="d-block mb-4 h-100">
-            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
-          </a>
-        </div>
-        <div class="col-lg-3 col-md-4 col-xs-6">
-          <a href="#" class="d-block mb-4 h-100">
-            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
-          </a>
-        </div>
-        <div class="col-lg-3 col-md-4 col-xs-6">
-          <a href="#" class="d-block mb-4 h-100">
-            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
-          </a>
-        </div>
-        <div class="col-lg-3 col-md-4 col-xs-6">
-          <a href="#" class="d-block mb-4 h-100">
-            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
-          </a>
-        </div>
-        <div class="col-lg-3 col-md-4 col-xs-6">
-          <a href="#" class="d-block mb-4 h-100">
-            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
-          </a>
-        </div>
-        <div class="col-lg-3 col-md-4 col-xs-6">
-          <a href="#" class="d-block mb-4 h-100">
-            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
-          </a>
-        </div>
-        <div class="col-lg-3 col-md-4 col-xs-6">
-          <a href="#" class="d-block mb-4 h-100">
-            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
-          </a>
-        </div>
-        <div class="col-lg-3 col-md-4 col-xs-6">
-          <a href="#" class="d-block mb-4 h-100">
-            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
-          </a>
-        </div>
-        <div class="col-lg-3 col-md-4 col-xs-6">
-          <a href="#" class="d-block mb-4 h-100">
-            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
-          </a>
-        </div>
-        <div class="col-lg-3 col-md-4 col-xs-6">
-          <a href="#" class="d-block mb-4 h-100">
-            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
-          </a>
-        </div>
-        <div class="col-lg-3 col-md-4 col-xs-6">
-          <a href="#" class="d-block mb-4 h-100">
-            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
-          </a>
-        </div>
+    <button type="button" class="btn dropdown-toggle" data-toggle="dropdown">Categories</button>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">Link 1</a>
+        <a class="dropdown-item" href="#">Link 2</a>
+        <a class="dropdown-item" href="#">Link 3</a>
       </div>
+  </div>
 
+  <div class="row text-center text-lg-left">
+
+    <div class="col-lg-3 col-md-4 col-xs-6">
+      <a href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+      </a>
     </div>
+    <div class="col-lg-3 col-md-4 col-xs-6">
+      <a href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+      </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-xs-6">
+      <a href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+      </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-xs-6">
+      <a href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+      </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-xs-6">
+      <a href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+      </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-xs-6">
+      <a href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+      </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-xs-6">
+      <a href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+      </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-xs-6">
+      <a href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+      </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-xs-6">
+      <a href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+      </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-xs-6">
+      <a href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+      </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-xs-6">
+      <a href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+      </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-xs-6">
+      <a href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+      </a>
+    </div>
+  </div>
+
+</div>
